634b05d34343d5bb9a295cce49c9fdabc750c9e3
35hudegang <1156389255@qq.com>
2015-02-05 16:34:31 +0800

    完成

--- java/maven/src/main/java/CompareNumber/Game.java
+++ java/maven/src/main/java/CompareNumber/Game.java
@@ -1,5 +1,7 @@
 package CompareNumber;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 /**
@@ -7,12 +9,33 @@ import java.io.PrintStream;
  */
 public class Game {
     private PrintStream out;
+    private final BufferedReader reader;
+    private final AnswerGenerator numberGenerator;
+    private final CompareNumber compareNumber;
 
-    public Game(PrintStream out) {
-    this.out=out;
+    public Game(PrintStream out, BufferedReader reader, AnswerGenerator numberGeneerator, CompareNumber compareNumber) {
+             this.out=out;
+        this.reader = reader;
+        this.numberGenerator = numberGeneerator;
+        this.compareNumber = compareNumber;
     }
-    public void start() {
-        System.out.print("Welcome!");
-        out.println("Please input your answer(6):");
+    public void start() throws IOException {
+        int chance=6;
+        out.println("Welcome!");
+        while(chance>0){
+            out.println("Please input your answer("+chance+"):");
+            String input =numberGenerator.generate();
+            String answer=reader.readLine();
+            String result=compareNumber.show(input,answer);
+            if ("4A0B".equals(result)){
+                out.println("Congratulations!");
+                return;
+            }
+            out.println(result);
+            chance--;
+        }
+        out.println("Game Over!");
+
+
     }
 }
--- java/maven/src/test/java/GameTest.java
+++ java/maven/src/test/java/GameTest.java
@@ -1,11 +1,15 @@
 import CompareNumber.AnswerGenerator;
+import CompareNumber.CompareNumber;
 import CompareNumber.Game;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashSet;
 
@@ -13,30 +17,127 @@ import java.util.HashSet;
  * Created by hu on 15-2-5.
  */
 public class GameTest {
-   /* @Test
-    public void game_out_welcome(){
-        String num=mock(String.class);
-        given(num)
-                .willReturn("welcome!");
-        Game game = new Game(num);
-        game.compareNumber();
-    }*/
+    private PrintStream out;
+    private Game game;
+    private InOrder inOrder;
+    private BufferedReader reader;
+    private AnswerGenerator numberGenerator;
+    private  CompareNumber compareNumber;
+
+    /* @Test
+        public void game_out_welcome(){
+            String num=mock(String.class);
+            given(num)
+                    .willReturn("welcome!");
+            Game game = new Game(num);
+            game.compareNumber();
+        }*/
+   @Before
+   public void  set_up() throws IOException {
+       out = mock(PrintStream.class);
+       reader=mock(BufferedReader.class);
+       numberGenerator=mock(AnswerGenerator.class);
+       compareNumber = new CompareNumber();
+       given(reader.readLine()).willReturn("1234");
+       given(numberGenerator.generate()).willReturn("4321");
+       game = new Game(out,reader,numberGenerator,compareNumber);
+       inOrder = inOrder(out);
+   }
     @Test
-    public void should_print_welcome_first_when_game_start(){
-        PrintStream out = mock(PrintStream.class);
-        Game game=new Game(out);
-        verify(out ,never()).print("Welcome!");
+    public void should_print_welcome_first_when_game_start() throws IOException {
+        verify(out ,never()).println("Welcome!");
         game.start();
         verify(out).println("Welcome!");
     }
     @Test
-    public void should_print_please_input_your_answer_second_when_game_start(){
-        PrintStream out = mock(PrintStream.class);
-        Game game=new Game(out);
+    public void should_print_please_input_your_answer_second_when_game_start() throws IOException {
         game.start();
-        InOrder inOrder=inOrder(out);
         inOrder.verify(out).println("Welcome!");
         inOrder.verify(out).println("Please input your answer(6):");
+    }
+    @Test
+    public void should_print_compare_result_when_answer_is_wrong() throws IOException {
+
+
+
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+        inOrder.verify(out).println("0A4B");
+    }
+    @Test
+    public void should_reduce_chance_one_when_answer_is_wrong() throws IOException {
+
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(5):");
+    }
+    @Test
+    public void reduce_chance_one_by_one_when_chance_is_0_game_over() throws IOException {
+
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(5):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(4):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(3):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(2):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(1):");
+
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Game Over!");
+        inOrder.verify(out,never()).println("0A4B");
+
+
+    }
+    @Test
+    public void should_congratulation_when_answer_is_right_first() throws IOException {
+        given(reader.readLine()).willReturn("4321");
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+        inOrder.verify(out).println("Congratulations!");
+        inOrder.verify(out,never()).println("4A0B");
+        inOrder.verify(out,never()).println("Please input your answer(5):");
+    }
+
+    @Test
+    public void should_congratulation_when_answer_is_right_second() throws IOException {
+        given(reader.readLine()).willReturn("6432","4321");
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+
+        inOrder.verify(out).println("Congratulations!");
+        inOrder.verify(out,never()).println("4A0B");
+        inOrder.verify(out,never()).println("Please input your answer(1):");
+    }
+    @Test
+    public void should_congratulation_when_answer_is_right_last() throws IOException {
+        given(reader.readLine()).willReturn("1234","1234","1234","1234","1234","4321");
+        game.start();
+        inOrder.verify(out).println("Welcome!");
+        inOrder.verify(out).println("Please input your answer(6):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(5):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(4):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(3):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(2):");
+        inOrder.verify(out).println("0A4B");
+        inOrder.verify(out).println("Please input your answer(1):");
+        inOrder.verify(out).println("Congratulations!");
+        inOrder.verify(out,never()).println("4A0B");
+        inOrder.verify(out,never()).println("Game Over!");
     }
 
 }
